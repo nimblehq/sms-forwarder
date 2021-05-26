53b7684de7099c63dd9b92e86f8e0b04a99a8438
Luong Vo <vodailuong@gmail.com>
2021-05-27 01:18:57 +0700

    feat: add mailer service + email template to send sms email

--- web/api/app.js
+++ web/api/app.js
@@ -1,19 +1,34 @@
 let express = require('express'),
   router = express.Router();
 
+let config = require('../config/config.json');
 let ApiExt = require('./api-ext');
 
+let mailer = require('../services/mailer');
+
 router.post('/v1/sms/forward', function (req, res) {
-  req.checkBody('sms_body').notEmpty().len({min: 1, max: 100});
+  req.checkBody('incoming_number').notEmpty().len({min: 1, max: 100});
+  req.checkBody('message_body').notEmpty().len({min: 1, max: 1000});
   req.checkBody('email').optional().len({min: 1, max: 1000}).isEmail();
   req.checkBody('slack_webhook').optional().len({min: 1, max: 1000}).isURL();
 
   req.sanitizeBody('email').toLowerCase();
 
   ApiExt.validateRequest(req, res, function () {
-    // TODO
-    let result = {};
-    ApiExt.buildSuccessResponse(res, result);
+    let binder = {
+      email: req.body.email,
+      subject: 'New SMS message - ' + config.app_name,
+      incoming_number: req.body.incoming_number,
+      message_body: req.body.message_body
+    };
+    mailer.send('forgot-password', binder, function (err, response, html, text) {
+      if (err) {
+        console.log(err);
+        ApiExt.buildFailedResponse(res, err.message);
+      } else {
+        ApiExt.buildSuccessResponse(res, 'The SMS message has been forwarded to ' + req.body.email);
+      }
+    });
   });
 });
 
