e5e21a8eaad8598c5ee36745bf3924f73bcd837c
Luong Vo <vodailuong@gmail.com>
2021-05-27 22:35:27 +0700

    feat: Setting subdir-heroku-buildpack to deploy subdirectory for web app on Heroku

--- web/deploy_heroku.sh
+++ web/deploy_heroku.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+
+# echo 'Setting subdir-heroku-buildpack'
+# heroku buildpacks:clear
+# heroku buildpacks:set https://github.com/timanovsky/subdir-heroku-buildpack
+# heroku buildpacks:add heroku/nodejs
+# heroku config:set PROJECT_PATH=web
+
 echo 'Deploying code'
 git push heroku master -f
 
-heroku run sequelize db:migrate --env production
+# heroku run sequelize db:migrate --env production
 
 echo 'Setting at least one running instance of the app'
 heroku ps:scale web=1
