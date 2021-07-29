c9b61a2929a723b5bc4b84643a5ea407885b1035
Luong Vo <luong@nimblehq.co>
2021-07-30 01:26:30 +0700

    [#27] Enable sequelize migration and fix nodejs sequelize-cli error on latest Nodejs versions

--- web/deploy_heroku.sh
+++ web/deploy_heroku.sh
@@ -9,7 +9,7 @@
 echo 'Deploying code'
 git push heroku master -f
 
-# heroku run sequelize db:migrate --env production
+heroku run sequelize db:migrate --env production
 
 echo 'Setting at least one running instance of the app'
 heroku ps:scale web=1
